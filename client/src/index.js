e96127e2b2f8c53997d6d37a7ea74eb81f23bd2c
carlos-ch <carlos.tschavez@gmail.com>
2021-05-15 09:03:37 +0200

    create cart context

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import { CartProvider } from './contexts/CartContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
