8631ff430e0dd956bc501e84c62d9da4207e9101
carlos-ch <carlos.tschavez@gmail.com>
2021-05-15 09:25:03 +0200

    add function to add products to cart

--- client/src/Components/productCard/ProductCard.js
+++ client/src/Components/productCard/ProductCard.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import './ProductCard.scss';
 
 const ProductCard = ({ data }) => {
+  const productId = data.id;
+  const [contextValue, setContext] = useContext(CartContext);
+  // add product {id , and quantity} to cart
+  const handleAddToCart = () => {
+    let updatedCart = [];
+    setContext(oldCart => {
+      const productIndex = oldCart.findIndex(i => i.id === productId);
+
+      /* if already in cart */
+      if (productIndex !== -1) {
+        updatedCart = [
+          ...oldCart.slice(0, productIndex),
+          { ...data, quantity: oldCart[productIndex].quantity + 1 },
+          ...oldCart.slice(productIndex + 1),
+        ];
+      } else {
+        /* if new to cart */
+        updatedCart = [...oldCart, { ...data, quantity: 1 }];
+      }
+      console.log(updatedCart);
+      return updatedCart;
+    });
+  };
+
   return (
     <article className="card product-card">
       <div className="card-inner-wrapper">
@@ -11,7 +36,9 @@ const ProductCard = ({ data }) => {
           <span className="card-price">$ {Math.floor(data.price)}</span>
           {/* <span className="card-price-badge">{data.discount}%</span> */}
         </div>
-        <button className="card-cta">Add to cart</button>
+        <button className="card-cta" onClick={handleAddToCart}>
+          Add to cart
+        </button>
       </div>
     </article>
   );
